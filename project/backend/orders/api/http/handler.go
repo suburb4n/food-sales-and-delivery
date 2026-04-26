95f9168e595dd9c9eb1f6461520291e1925799e8
dluis <dluis1651@gmail.com>
2026-04-26 22:46:55 +0100

    feat:API specs for customer registration

--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -13,5 +13,14 @@ func NewHandler() Handler {
 }
 
 func Register(ctx context.Context, e common.EchoRouter, handler Handler) error {
+	RegisterHandlers(e, NewStrictHandler(handler, nil))
 	return nil
+}
+
+func (h Handler) RegisterCustomer(ctx context.Context, request RegisterCustomerRequestObject) (RegisterCustomerResponseObject, error) {
+	id := common.NewUUIDv7()
+
+	return RegisterCustomer201JSONResponse{
+		CustomerUuid: id,
+	}, nil
 }
